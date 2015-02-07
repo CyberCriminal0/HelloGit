7c796662195183c3e1a93abcd328426945fa7d3c
Jack Eller <admin@kilobear.com>
2015-02-06 20:57:20 -0500

    Asks for name

--- src/hello.java
+++ src/hello.java
@@ -1,10 +1,21 @@
 
-package hello;
+//written by Jack Eller
+
+import java.util.Scanner;
+
 public class hello{
 public static void main(String[] args){
 
 
     System.out.println("Hello World!");
+    System.out.println("Please input your name");
+    
+    Scanner scan = new Scanner(System.in);
+
+    String name = scan.nextLine();
+
+    System.out.println("Hello " + name + " this is Java");
+
 
 
 }
